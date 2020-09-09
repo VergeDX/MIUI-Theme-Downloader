ec0d16f899117649b51fe2d6321a1825aa50ef8e
VergeDX <neko@hydev.org>
2020-09-09 09:18:14 +0800

    [+] allow specific miui version v[10 - 12]

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 // Import yargs lib, import using Destructuring assignment.
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-const {argv} = require('yargs')
+const {program} = require('commander');
 
-// Args needed a url param.
-// If not, exit with code -1.
-if (argv._.length !== 1) {
-    console.log("Usage: miui-theme-downloader [theme_url]. ")
+// https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md#%E9%80%89%E9%A1%B9
+program.option('-u --url <string>', '[required] miui theme market share url. ')
+    .option('-m --miuiVersion <string>', '[optional] specific miui version in v[10 - 12]. ', 'v12')
+program.parse(process.argv)
+
+// Required url args missing, show help & exit process with -1.
+if (program.url === undefined) {
+    console.log('Please see miui-theme-downloader --help. ')
     process.exit(-1)
 }
 
-// miui theme link url, example:
+// miui theme token, example: miui theme link is:
 // http://zhuti.xiaomi.com/detail/fffc9612-2682-4e61-b390-0504ff154644
-let input_url = argv._[0]
-let theme_token = input_url.split('/')[4]
-
+// token is: fffc9612-2682-4e61-b390-0504ff154644
+const theme_token = program.url.split('/')[4]
 const API_BASE = 'https://thm.market.xiaomi.com/thm/download/v2/'
 
 // https://nodejs.org/api/querystring.html
 const querystring = require('querystring');
 const fetch = require('node-fetch')
 
-let fetch_url = `${API_BASE}${theme_token}?` + querystring.encode({miuiVersion: 'V12'})
+const miui_version = program.miuiVersion
+const fetch_url = `${API_BASE}${theme_token}?` + querystring.encode({miuiVersion: miui_version})
 fetch(fetch_url).then(r => r.json().then(json => {
     // apiCode != 0,
     if (json.apiCode !== 0) {
